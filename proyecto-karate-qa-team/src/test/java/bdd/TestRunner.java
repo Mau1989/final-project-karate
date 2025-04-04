f7065e0ae7ac6c0bee3d7b7799089c02e5efe35c
Mau1989 <mauricio.salomon1@MauriciomonsMBP.lan>
2025-04-03 22:56:07 -0600

    version casi final

--- proyecto-karate-qa-team/src/test/java/bdd/TestRunner.java
+++ proyecto-karate-qa-team/src/test/java/bdd/TestRunner.java
@@ -3,13 +3,32 @@ package bdd;
 import com.intuit.karate.junit5.Karate;
 
 public class TestRunner {
-    @Karate.Test
-    Karate testLogin() {
-        return Karate.run("loginAuth").relativeTo(getClass());
-    }
 
     @Karate.Test
     Karate testRegister() {
-        return Karate.run("registerAuth").relativeTo(getClass());
+        return Karate.run("auth/registerAuth").relativeTo(getClass());
     }
+
+    @Karate.Test
+    Karate testLogin() {
+        return Karate.run("auth/loginAuth").relativeTo(getClass());
+    }
+    @Karate.Test
+    Karate testAddProduct() {
+        return Karate.run("product/addProduct").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testGetByProductId() {
+        return Karate.run("product/getByProductId").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testUpdateProduct() {
+        return Karate.run("product/updateProduct").relativeTo(getClass());
+    }
+
+
+
+
 }
